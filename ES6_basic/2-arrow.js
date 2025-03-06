2dfe631ac3ce8fb79c848ff874622fd7af23e784
Frank Grove <fragro@gmail.com>
2025-03-06 14:36:55 -0600

    jad to update the arrow fuction witht the correct varibles

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,6 +1,13 @@
-// 2-main.js (unchanged)
-import getNeighborhoodsList from './2-arrow.js';
+// 2-arrow.js (Alternative using an arrow function *internally*)
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+  this.addNeighborhood = function(newNeighborhood) {
+    // Use an arrow function *inside* the method, if needed
+    const addToList = (item) => {
+      this.sanFranciscoNeighborhoods.push(item); // 'this' refers to the getNeighborhoodsList instance
+      return this.sanFranciscoNeighborhoods;
+    };
+    return addToList(newNeighborhood);
+  };
+}
