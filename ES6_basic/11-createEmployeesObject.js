13ab99e359630e363b0796259f597a54fc52480f
Frank Grove <fragro@gmail.com>
2025-03-06 17:06:33 -0600

    had to update file

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -2,7 +2,8 @@
  * Creates an object representing employees within a department.
  * @param {string} departmentName - The name of the department.
  * @param {string[]} employees - An array of employee names.
- * @returns {object} - An object with the department name as the key and an array of employees as the value.
+ * @returns {object} - An object with the department name as the key and
+ * an array of employees as the value.
  */
 export default function createEmployeesObject(departmentName, employees) {
   const employeesObject = {};
