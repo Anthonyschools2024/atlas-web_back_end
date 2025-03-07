fb19dbbc90deb6eea74154dbf81c3a1f11a4e37d
Frank Grove <fragro@gmail.com>
2025-03-06 22:51:30 -0600

    had to make an update to an element

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,42 +1,5 @@
-// Pricing
+// pricing
 import Currency from './3-currency.js';
-
-export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
-
-  // Getter for amount
-  get amount() {
-    return this._amount;
-  }
-
-  // Setter for amount
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  // Getter for currency
-  get currency() {
-    return this._currency;
-  }
-
-  // Setter for currency
-  set currency(newCurrency) {
-    this._currency = newCurrency;
-  }
-
-  // Method to display full price
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-  }
-
-  // Static method to convert price
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
-}import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(amount, currency) {
