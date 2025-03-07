02d0ab25786d808ddd2e2a63be11ce39593734a2
Frank Grove <fragro@gmail.com>
2025-03-06 20:00:55 -0600

    had to make update

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,39 @@
 // 4-pricing.js
+import Currency from './3-currency'; // Removed .js extension
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters and Setters (grouped)
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}// 4-pricing.js
 import Currency from './3-currency.js';
 
 export default class Pricing {
