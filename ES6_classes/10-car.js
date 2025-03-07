a925f277a36fca7191ddf8c62a4d535d3b68c7d6
Frank Grove <fragro@gmail.com>
2025-03-07 00:25:56 -0600

    impelement the class named car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,42 @@
+// car
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Getter for brand
+  get brand() {
+    return this._brand;
+  }
+
+  // Setter for brand
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  // Getter for motor
+  get motor() {
+    return this._motor;
+  }
+
+  // Setter for motor
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  // Getter for color
+  get color() {
+    return this._color;
+  }
+
+  // Setter for color
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
