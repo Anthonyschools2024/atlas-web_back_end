2b06e981bd98d444be97d325559f8211ddfe7ec0
Frank Grove <fragro@gmail.com>
2025-03-07 00:19:53 -0600

    corrected a line count error

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-// hoisting
+// 9-hoisting.js
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -42,7 +42,6 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
