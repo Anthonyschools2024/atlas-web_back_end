90250d435d910d8730897209484d1dc416da35cc
Frank Grove <fragro@gmail.com>
2025-03-06 23:27:27 -0600

    implement class name SkyHighBuilding

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+// 6-sky_high.js
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the constructor of the parent class (Building) with sqft
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
