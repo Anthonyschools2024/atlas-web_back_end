2843087fb5fbfbf80d6b2c08a7f95e8448f1be72
Frank Grove <fragro@gmail.com>
2025-03-07 00:11:36 -0600

    hopefully last needed update for the linter in this task

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -18,7 +18,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    this._holbertonClass = holbertonClass; // Corrected: Accept holbertonClass as parameter and assign
   }
 
   get fullName() {
@@ -26,11 +26,11 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this._holbertonClass; // Corrected: Return _holbertonClass
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Corrected: Use 'this' instead of 'self'
   }
 }
 
@@ -44,7 +44,5 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 
-const listOfStudents = [student1, student2, student3, student4, student5]; // Define listOfStudents
-export default listOfStudents; // Default export it.
-
-`
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
