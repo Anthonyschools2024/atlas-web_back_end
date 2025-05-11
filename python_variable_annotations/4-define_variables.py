7ddaf6b31de9ccdf5b37a4067fdbc84855486ae6
Frank Grove <fragro@gmail.com>
2025-05-11 16:27:13 -0500

    definitions and type annotations

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates several variables:
+a: an integer
+pi: a float
+i_understand_annotations: a boolean
+school: a string
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"#!/usr/bin/env python3
+"""
+This module defines and annotates several variables:
+a: an integer
+pi: a float
+i_understand_annotations: a boolean
+school: a string
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
