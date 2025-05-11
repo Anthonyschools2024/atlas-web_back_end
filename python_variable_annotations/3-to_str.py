c9678aadf9dabfb605d5be1857ebf0d42262166d
Frank Grove <fragro@gmail.com>
2025-05-11 13:28:31 -0500

    annotation must be a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function 'to_str'.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+
+    Args:
+        n (float): The float number to convert.
+
+    Returns:
+        str: The string representation of n.
+    """
+    return str(n)
