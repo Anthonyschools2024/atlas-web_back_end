305bd62e7b95ce28bd471a7ce675d5b34fd84bd5
Frank Grove <fragro@gmail.com>
2025-05-11 22:35:48 -0500

    had to update my syntax

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,12 @@ This module contains a coroutine that uses async comprehension.
 """
 
 from typing import List
-from 0-async_generator import async_generator
+
+# Import async_generator using __import__ due to the leading digit in the module name
+async_generator_module = __import__('0-async_generator')
+async_generator = async_generator_module.async_generator
+# Or, more directly, though slightly less readable for some:
+# async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
