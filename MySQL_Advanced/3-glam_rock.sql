304996ba324850fd14e154d59d643ee91ce589df
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-06-10 21:52:49 -0500

    updated task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,11 @@
+
 -- This script lists all bands with 'Glam rock' as their main style,
 -- ranked by their longevity.
 -- The lifespan is calculated using the 'formed' and 'split' years.
--- For bands that are still active, 2022 is used as the current year.
+-- For bands that are still active, 2024 is used as the current year.
 SELECT
     band_name,
-    (IFNULL(split, 2022) - formed) AS lifespan
+    (IFNULL(split, 2024) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
