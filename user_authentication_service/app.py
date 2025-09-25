46cf3e03e91f2193b68d8a0abd5052f3650305ef
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-09-24 22:24:23 -0500

    login routes

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 This module sets up a basic Flask application.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -42,6 +42,31 @@ def users() -> str:
     except ValueError:
         payload = {"message": "email already registered"}
         return jsonify(payload), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """
+    Handles the POST request for the /sessions endpoint to log a user in.
+
+    Form data:
+        - email (str): The user's email address.
+        - password (str): The user's password.
+
+    Returns:
+        str: A JSON payload indicating success or an aborts with a 401 status.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
